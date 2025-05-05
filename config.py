07d3409613bea194f1caf2bbe8f4a0166e8c41b9
KPVivek18 <kayagurala.vivek@gmail.com>
2025-05-05 13:53:49 -0500

    Update config.py

--- config.py
+++ config.py
@@ -1,17 +1,13 @@
 import os
 from dotenv import load_dotenv
 
-# Load .env file values into environment variables
 load_dotenv()
 
 class Config:
-    SECRET_KEY = 'your-secret-key'  # Needed for Flask-Login
-
-    # MySQL connection string
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:Visca10$@localhost/task_manager'
+    SECRET_KEY = os.getenv('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
-    # AWS credentials from .env
     AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
     AWS_BUCKET_NAME = os.getenv('AWS_BUCKET_NAME')
